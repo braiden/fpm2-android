9ba536eea71b434aab88d7c35851fb0a377583d6
(no author) <(no author)@1cc42611-e091-430a-a3cd-04b17827a907>
2010-08-27 01:50:07 +0000

    - update fonts to use android text preferences

--- app/src/org/braiden/fpm2/PasswordItemListActivity.java
+++ app/src/org/braiden/fpm2/PasswordItemListActivity.java
@@ -54,7 +54,8 @@ public class PasswordItemListActivity extends FpmListActivity {
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);    	
+    	super.onCreate(savedInstanceState);
+    	//setContentView(R.layout.password_item_list);
     	// register the adapter for building view for each element of our list
     	BaseAdapter adapter = new FpmCryptListAdapter(this);
     	setListAdapter(adapter);
